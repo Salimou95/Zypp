7008f0cb7947a645270921ff22cd1e12180fdecd
Salimou95 <salimoudiaby95@gmail.com>
2025-09-16 10:24:16 +0200

    make auth

--- app/View/partials/nav.php
+++ app/View/partials/nav.php
@@ -1,3 +1,6 @@
+<?php
+if (session_status() === PHP_SESSION_NONE) session_start();
+?>
 <nav class="navbar navbar-expand-lg shadow-sm mb-4">
   <div class="container-fluid">
     <a class="navbar-brand d-flex align-items-center" href="/Zypp/public/">
@@ -14,6 +17,11 @@
         <li class="nav-item">
           <a class="nav-link fw-bold text-primary" href="/Zypp/public/contact">Contact</a>
         </li>
+        <?php if (isset($_SESSION['user'])): ?>
+        <li class="nav-item">
+          <a class="nav-link fw-bold text-danger" href="/Zypp/auth/logout">Se déconnecter</a>
+        </li>
+        <?php endif; ?>
       </ul>
     </div>
   </div>
