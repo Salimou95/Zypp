a6174b41244bbe5730786d9c59cabf4a95f968c6
Salimou95 <salimoudiaby95@gmail.com>
2025-09-17 19:32:13 +0200

    correction footer

--- app/View/partials/footer.php
+++ app/View/partials/footer.php
@@ -1,5 +1,52 @@
-<footer class="footer-site mt-auto">
-    <div class="container text-center py-3">
-        <span class="text-white">&copy; 2025 Zypp. Tous droits réservés.</span>
+<footer class="footer">
+  <!-- Premier article avec fond vert comme la nav et icônes sociales -->
+  <article class="footer-social">
+    <div class="container">
+      <div class="social-icons-container">
+        <a href="#" class="footer-social-icon"><i class="fab fa-facebook-f"></i></a>
+        <a href="#" class="footer-social-icon"><i class="fab fa-twitter"></i></a>
+        <a href="#" class="footer-social-icon"><i class="fab fa-youtube"></i></a>
+        <a href="#" class="footer-social-icon"><i class="fab fa-pinterest"></i></a>
+        <a href="#" class="footer-social-icon"><i class="fab fa-instagram"></i></a>
+      </div>
     </div>
+  </article>
+
+  <!-- Second article avec fond bleu -->
+  <article class="footer-info">
+    <div class="container">
+      <div class="row justify-content-center g-0">
+        <!-- Premier article : A propos de ZYPP -->
+        <div class="col-auto">
+          <h5>A propos de ZYPP</h5>
+          <ul class="footer-links-no-bullet">
+            <li><a href="#">La Communauté ZYPP</a></li>
+            <li><a href="#">Le vélo électrique</a></li>
+            <li><a href="#">La trottinette électrique</a></li>
+            <li><a href="#">La durabilité</a></li>
+            <li><a href="#">Nos innovations</a></li>
+            <li><a href="#">La sécurité</a></li>
+          </ul>
+        </div>
+
+        <!-- Deuxième article : Nos programmes -->
+        <div class="col-auto">
+          <h5>Nos programmes</h5>
+          <ul class="footer-links-with-bullet">
+            <li><a href="#">Publicité</a></li>
+            <li><a href="#">Lime Access</a></li>
+            <li><a href="#">Lime Hero</a></li>
+            <li><a href="#">Lime Assist</a></li>
+            <li><a href="#">Lime for Business</a></li>
+            <li><a href="#">Assurance</a></li>
+            <li><a href="#">Nos villes</a></li>
+          </ul>
+        </div>
+      </div>
+      <hr class="footer-divider">
+      <div class="text-center">
+        <p>&copy; 2024 ZYPP. Tous droits réservés.</p>
+      </div>
+    </div>
+  </article>
 </footer>
